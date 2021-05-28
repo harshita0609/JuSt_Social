99147e31c219a0152993ad34db439994e8903c70
harshita0609 <harshitajainjindal@gmail.com>
2021-05-28 21:17:59 +0530

    partials header footer added

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,8 @@
 module.exports.profile = function(req,res){
-    return res.end('<h1>USER controller -> profile</h1>');
+    // return res.end('<h1>USER controller -> profile</h1>');
+
+    return res.render('profile.ejs',{
+        title: "Profile"
+    });
 };
 
