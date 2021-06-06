6b803ee4bc5ff2ce794d2effe9ed5f01428fc6ef
harshita0609 <harshitajainjindal@gmail.com>
2021-06-06 18:12:34 +0530

    not allowing to go to sign in or sign up page after login

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -9,6 +9,12 @@ module.exports.profile = function(req,res){
 };
 
 module.exports.signUp = function(req,res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+
     return res.render('user_sign_up',{
         title: "Just_Social|SignUp"
     });
@@ -16,6 +22,12 @@ module.exports.signUp = function(req,res){
 
 
 module.exports.signIn = function(req,res){
+
+    
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in',{
         title: "Just_Social|SignIn"
     });
